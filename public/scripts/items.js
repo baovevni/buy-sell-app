d26064683868bca27c93367ad9f3565629792496
ihleung <ihfungleung@gmail.com>
2024-06-02 10:52:54 -0400

    add the header item and items js files

---
+++ public/scripts/items.js
@@ -0,0 +1,30 @@
+$(() => {
+
+  const $itemListings = $(`
+  <section class="item-listings" id="item-listings">
+      <p>Loading...</p>
+    </section>
+  `);
+  window.$itemListings = $itemListings;
+
+  window.itemListings = {};
+
+  function addListing(listing) {
+    $itemListings.append(listing);
+  }
+  function clearListings() {
+    $itemListings.empty();
+  }
+  window.itemListings.clearListings = clearListings;
+
+  function addItem(items, isFavourite = false) {
+    clearListings();
+    for (const itemId in items) {
+      const item = items[itemId];
+      const listing = itemListing.createListing(item, isFavourite);
+      addListing(listing);
+    }
+  }
+  window.itemListings.addItem = addItems;
+
+}); 
