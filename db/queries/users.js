7645888a8e02501fdcbf00b3df6f14c86ebdfb57
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-03 17:24:44 -0400

    Revert "Updated the login route"

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,49 +1,10 @@
 const db = require('../connection');
 
-// CREATE
-const addUser = function(name, email, password, phone) {
-  return db.query(
-    `INSERT
-    INTO users
-    (name, email, password, phone)
-    VALUES
-    ($1, $2, $3, $4)
-    RETURNING *;
-    `, [name, email, password, phone])
+const getUsers = () => {
+  return db.query('SELECT * FROM users;')
     .then(data => {
-      return data.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
+      return data.rows;
     });
 };
 
-// READ by id
-const getUserById = function(id) {
-  return db.query(
-    `SELECT *
-    FROM users
-    WHERE id = $1;`, [id])
-    .then(data => {
-      return data.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
-
-// READY by email
-const getUserByEmail = function(email) {
-  return db.query(
-    `SELECT *
-    FROM users
-    WHERE email = $1;`, [email])
-    .then(data => {
-      return data.rows[0];
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
-
-module.exports = { getUserById, getUserByEmail, addUser };
+module.exports = { getUsers };
---
+++ routes/apiRoutes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const database = require("../db/database");
+
+const router = express.Router();
+
+router.get("/items", (req, res) => {
+  database
+    .getAllProperties(req.query, 20)
+    .then((items) => res.send({ items }))
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
+});
+
+router.get("/favourites", (req, res) => {
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.send({ error: "error" });
+  }
+
+  database
+    .getAllFavourites(userId)
+    .then((favourites) => res.send({ favourites }))
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
+});
+
+router.post("/items", (req, res) => {
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.send({ error: "error" });
+  }
+
+  const newItem = req.body;
+  newItem.user_id = userId;
+  database
+    .addItem(newItem)
+    .then((item) => {
+      res.send(item);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
+});
+
+module.exports = router;
