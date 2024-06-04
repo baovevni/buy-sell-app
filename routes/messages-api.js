421468be970103cc9ae9801c093c013fdbbebcfb
ihleung <ihfungleung@gmail.com>
2024-06-04 12:04:06 -0400

    add the message-api

--- routes/messages-api.js
+++ routes/messages-api.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const userQueries = require('../db/queries/items');
+
+router.get('/', (req, res) => {
+  userQueries.getItems()
+    .then(items => {
+      res.json(items);
+    });
+});
+
+router.post('/', (req, res) => {
+  userQueries.addItem(req.body.item)
+    .then(() => {
+      res.json({ message: 'Item added' });
+    });
+});
+
+router.post('/:id/delete', (req, res) => {
+  userQueries.deleteItem(req.params.id)
+    .then(() => {
+      res.json({ message: 'Item deleted' });
+    });
+});
+
+
 
 module.exports = router;
