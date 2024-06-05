f986623b933312a372497cdd1347fc7a3018c406
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-05 15:57:34 -0400

    add messages functionality

--- routes/messages-api.js
+++ routes/messages-api.js
@@ -5,22 +5,26 @@ const userQueries = require('../db/queries/messages');
 router.get('/', (req, res) => {
   userQueries.getMessages(req.session.userId)
     .then( messages => {
-      console.log(messages);
       res.render('messages', { user: req.session.userId, messages: messages} );
     });
 });
 
 router.post('/', (req, res) => {
-  userQueries.addItem(req.body.item)
+  const text = req.body.text;
+  const item_id = req.body.item_id;
+  const owner_id = req.body.owner_id;
+  userQueries.addMessage(text, item_id, owner_id)
     .then(() => {
-      res.json({ message: 'Item added' });
+      res.redirect("/main");
     });
 });
 
-router.post('/:id/delete', (req, res) => {
-  userQueries.deleteItem(req.params.id)
+router.post('/delete', (req, res) => {
+  const owner_id = req.body.user_id;
+  const message_id = req.body.message_id;
+  userQueries.deleteMessage(owner_id, message_id)
     .then(() => {
-      res.json({ message: 'Item deleted' });
+      res.redirect("/messages");
     });
 });
 
